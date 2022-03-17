fa5a5534a3cfb51534060350bb5f8bb49f4be885
Mohab <khalifamohab97@outlook.com>
2022-03-17 20:18:18 +0000

    MK: Updated Car Class

--- src/com/qa/garage/Car.java
+++ src/com/qa/garage/Car.java
@@ -1,5 +1,55 @@
 package com.qa.garage;
 
-public class Car {
+public class Car extends Vehicle {
+
+	// Fields
+	private String make;
+	private String model;
+	private String bodyType;
+
+	// Method
+	@Override
+	public void insertKey() {
+		System.out.println("VROOM VROOM");
+	}
+
+	// Getters & Setters
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getBodyType() {
+		return bodyType;
+	}
+
+	public void setBodyType(String bodyType) {
+		this.bodyType = bodyType;
+	}
+
+	// Constructor
+	public Car(int id, String travelType, int numOfSeats, boolean electric, String colour, int maxSpeed,
+			boolean damaged, String make, String model, String bodyType) {
+		super(id, travelType, numOfSeats, electric, colour, maxSpeed, damaged);
+		this.make = make;
+		this.model = model;
+		this.bodyType = bodyType;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [make=" + make + ", model=" + model + ", bodyType=" + bodyType + "]";
+	}
 
 }
