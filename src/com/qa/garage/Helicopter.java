fd5453c275b3dc29108b0a8919ef7113b29bdada
Mohab <khalifamohab97@outlook.com>
2022-03-17 20:24:28 +0000

    MK: Updated Helicopter Class

--- src/com/qa/garage/Helicopter.java
+++ src/com/qa/garage/Helicopter.java
@@ -1,5 +1,46 @@
 package com.qa.garage;
 
-public class Helicopter {
+// Child Class
+public class Helicopter extends Vehicle {
+
+	// Fields
+	private int numOfPropellers;
+	private boolean chopper;
+
+	// Method
+	public void airbourne() {
+		System.out.println("* chu chu chu chu*");
+	}
+
+	// Getters & Setters
+	public int getNumOfPropellers() {
+		return numOfPropellers;
+	}
+
+	public void setNumOfPropellers(int numOfPropellers) {
+		this.numOfPropellers = numOfPropellers;
+	}
+
+	public boolean isChopper() {
+		return chopper;
+	}
+
+	public void setChopper(boolean chopper) {
+		this.chopper = chopper;
+	}
+
+	// Constructor
+	public Helicopter(int id, String travelType, int numOfSeats, boolean electric, String colour, int maxSpeed,
+			boolean damaged, int numOfPropellers, boolean chopper) {
+		super(id, travelType, numOfSeats, electric, colour, maxSpeed, damaged);
+		this.numOfPropellers = numOfPropellers;
+		this.chopper = chopper;
+	}
+
+	// toString
+	@Override
+	public String toString() {
+		return "Helicopter [numOfPropellers=" + numOfPropellers + ", chopper=" + chopper + "]";
+	}
 
 }
